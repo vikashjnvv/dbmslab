2a3c24fee5e7a392d8e8e56944d687555bcb5935
Vikash Kumar <198427890+vikashjnvv@users.noreply.github.com>
2025-05-10 22:27:28 +0530

    Question6

---
+++ q6.py
@@ -0,0 +1,15 @@
+from mysql.connector import connect
+conn = connect(
+host="localhost",
+user="root",
+port=3307,
+password="123456",
+database="lab"
+)
+cur = conn.cursor()
+cur.execute(""" 
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'new_user';
+""")
+conn.commit()
+conn.close()
+print("All privileges and grant option revoked from 'new_user'.")
